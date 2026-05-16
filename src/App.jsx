a99dd3e555ef9b83d25e24a7d58f2db55e5d87cb
RITIK YADAV <ritiky453@gmail.com>
2026-05-16 17:30:44 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white p-6">
+      <h1 className="text-4xl font-bold text-center">
+        Gully Cricket Dashboard
+      </h1>
+    </div>
+  )
+}
